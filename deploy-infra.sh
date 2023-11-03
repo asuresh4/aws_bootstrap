ba65a5ba82e56a83e6fa9394876c24810ce5a512
Akash Suresh <akashsb15@gmail.com>
2023-11-03 03:10:36 -0400

    Add CodeDeploy/CodeBuild/CodePipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -8,6 +8,10 @@ CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 EC2_INSTANCE_TYPE=t2.micro
 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=main
 
 # Deploys static resources
 echo -e "\n\n======================== Deploying setup.yml ============================"
@@ -24,7 +28,21 @@ aws cloudformation deploy \
 
 # Deplpy the CloudFormation template
 echo -e "\n\n======================== Deploying main.yml ============================"
-aws cloudformation deploy --region $REGION  --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file main.yml --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+aws cloudformation deploy \
+  --region $REGION  \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+  
 
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports --profile awsbootstrap  --query "Exports[?Name=='InstanceEndpoint'].Value"
