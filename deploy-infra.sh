3c1fad1f70e87b33d1556c269c0e538556c70117
Akash Suresh <akashsb15@gmail.com>
2023-11-15 22:52:35 -0500

    Reaarange for nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,7 @@ REGION=us-east-2
 CLI_PROFILE=awsbootstrap
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
 
 EC2_INSTANCE_TYPE=t2.micro
 
@@ -26,6 +27,23 @@ aws cloudformation deploy \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 
+# Package CloudFormation templates into an S3 bucket
+echo -e "\n\n======================== Packaging main.yml ============================"
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:"
+  echo $PACKAGE_ERR
+  exit 1
+fi
+
 # Deplpy the CloudFormation template
 echo -e "\n\n======================== Deploying main.yml ============================"
 aws cloudformation deploy \
@@ -41,7 +59,8 @@ aws cloudformation deploy \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
   
 
 if [ $? -eq 0 ]; then
